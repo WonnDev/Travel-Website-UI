067fc24fed6a6422dcce6dcf4c1b3114ebb519aa
Wonn <wonndev@gmail.com>
2022-11-23 18:44:31 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /*index*/
-$('.single-item').slick();
 $('.slick-view').slick({
     dots: false,
     infinite: false,
@@ -30,6 +29,68 @@ $('.slick-view').slick({
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.slick-row').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    button: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.slick-img-background').slick({
+    centerMode: true,
+    centerPadding: '420px',
+    slidesToShow: 1,
+    button: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        button: false,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        button: false,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
         }
       }
     ]
