98d26d18bffaa6e9294897b00496eb2283e83f58
Wonn <wonndev@gmail.com>
2022-11-24 18:15:38 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*index*/
-$('.slick-view').slick({
+  $('.slick-view').slick({
     dots: false,
     infinite: false,
     speed: 300,
@@ -91,6 +91,40 @@ $('.slick-view').slick({
           centerMode: true,
           centerPadding: '40px',
           slidesToShow: 1
+        }
+      }
+    ]
+  });
+  $('.list-top-rate').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false,
+    button: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       }
     ]
